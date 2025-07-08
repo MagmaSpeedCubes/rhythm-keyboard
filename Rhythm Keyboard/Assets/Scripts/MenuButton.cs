4677aaf1c5323202c421756856bad97901e2b165
MagmaSpeedCubes <110320002+MagmaSpeedCubes@users.noreply.github.com>
2025-07-08 10:24:41 -0700

    ui tests

--- Rhythm Keyboard/Assets/Scripts/MenuButton.cs
+++ Rhythm Keyboard/Assets/Scripts/MenuButton.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
-
-public class MenuButton
+using UnityEngine.UI;
+public class MenuButton : MonoBehaviour
 {
-    
+    [SerializeField] private Canvas startCanvas;
+    [SerializeField] private Canvas openCanvas;
+
+    public void onClick(){
+        startCanvas.enabled = false;
+        openCanvas.enabled = true;
+    }
 }
